735786a6b51546aaf85677002d7abf5d43dca876
Shivam Singh <shivamsingh4458@gmail.com>
2022-02-03 08:51:37 +0530

    Normal Comands in TypeScript

---
+++ intro.js
@@ -0,0 +1,6 @@
+/*
+    Normal Comands in TypeScript-
+    tsc -v  for checking Type Script Version
+    tsc <file name>   for run typescript file
+    tsc <file name> --watch for countinu watching files
+*/

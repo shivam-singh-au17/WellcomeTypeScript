a943c19922675f81e3ce8d13455ac27e8f79d4a6
Shivam Singh <shivamsingh4458@gmail.com>
2022-02-03 08:55:11 +0530

    ARRAY in TypeScript

--- intro.js
+++ intro.js
@@ -6,7 +6,7 @@ exports.__esModule = true;
     tsc <file name>   for run typescript file
     tsc <file name> --watch for countinu watching files
 */
-// Intro of typescript and run First Code
+// Intro of typescript
 var str = "Shivam Singh";
 console.log("str:", str);
 console.warn("str:", str);
@@ -29,3 +29,27 @@ var str1 = "Shivam";
 console.log(str1);
 var isFine = true;
 console.log(isFine);
+// ARRAY in TypeScript
+var strArr = ["shivam", "Singh", "number"]; // default data type STRING because all eliment is STRING type
+strArr.push("name");
+/*
+strArr.push(10); // throw error because all eliment is STRING type
+*/
+console.log(strArr);
+var strArrDataType = ["shivam", "Singh", "number"];
+console.log("strArrDataType:", strArrDataType);
+var numArr = [2, 3, 5]; // default data type NUMBER because all eliment is NUMBER type
+numArr.push(7);
+/*
+numArr.push("str"); // throw error because all eliment is NUMBER type
+*/
+console.log(numArr);
+var numArrDataTye = [2, 3, 5];
+console.log("numArrDataTye:", numArrDataTye);
+var mixTypeArr = [4, 5, "name", "num", true, false, 2.2]; // in this ARRAY all types of data avalable so that it will not throw error
+mixTypeArr.push(100);
+mixTypeArr.push("string value");
+mixTypeArr.push(true);
+console.log(mixTypeArr);
+var boolDataType = [true, false];
+console.log("boolDataType:", boolDataType);
--- intro.ts
+++ intro.ts
@@ -8,7 +8,7 @@ export {};
 */
 
     
-// Intro of typescript and run First Code
+// Intro of typescript
 
 let str = "Shivam Singh";
 console.log("str:", str);
@@ -36,3 +36,33 @@ console.log(str1);
 
 const isFine: boolean = true;
 console.log(isFine);
+
+// ARRAY in TypeScript
+
+let strArr = ["shivam", "Singh", "number"]; // default data type STRING because all eliment is STRING type
+strArr.push("name");
+/*
+strArr.push(10); // throw error because all eliment is STRING type
+*/
+console.log(strArr);
+let strArrDataType: string[] = ["shivam", "Singh", "number"];
+console.log("strArrDataType:", strArrDataType);
+
+let numArr = [2, 3, 5]; // default data type NUMBER because all eliment is NUMBER type
+numArr.push(7);
+/*
+numArr.push("str"); // throw error because all eliment is NUMBER type
+*/
+console.log(numArr);
+let numArrDataTye: number[] = [2, 3, 5];
+console.log("numArrDataTye:", numArrDataTye);
+
+let mixTypeArr = [4, 5, "name", "num", true, false, 2.2]; // in this ARRAY all types of data avalable so that it will not throw error
+mixTypeArr.push(100);
+mixTypeArr.push("string value");
+mixTypeArr.push(true);
+console.log(mixTypeArr);
+
+let boolDataType: boolean[] = [true, false];
+console.log("boolDataType:", boolDataType);
+
